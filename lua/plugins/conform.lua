130e7795e6e3fa9847bd3c4abef8afadd4e5a867
Adrian Gielniewski <adrian.gielniewski@gmail.com>
2024-01-04 20:09:24 +0100

    Configure python formatting
    Use isort and black.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { { "isort", "black" } },
+      },
+      formatters = {
+        isort = {
+          prepend_args = { "--profile", "black" },
+        },
+      },
+    },
+  },
+}
