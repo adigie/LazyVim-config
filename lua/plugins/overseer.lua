ac10d0f74ef0f16d93f284c4e8596e3f671530bc
Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>
2025-11-03 15:44:52 +0100

    overseer
    Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>ol", "<cmd>OverseerLoadBundle<cr>", desc = "Load bundle" },
+      { "<leader>or", "<cmd>OverseerRunCmd<cr>", desc = "Run cmd" },
+    },
+  },
+}
